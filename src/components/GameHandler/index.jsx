d73750db2cdad966f4c0fde95dd318b067efe78f
Zubiik <elodiefrappat08@gmail.com>
2023-09-01 11:46:32 +0200

    feat/win loop

--- src/components/GameHandler/index.jsx
+++ src/components/GameHandler/index.jsx
@@ -14,27 +14,43 @@ const GameHandler = () => {
     gameLoop();
   }, [data]);
 
-  const gameLoop = () => {
-    console.log(data.length);
+  const winCount = (gamerResult, resultToCompare) => {
+    console.log('gamerResult ->',gamerResult,'resultToCompare ->', resultToCompare );
+    if (gamerResult > resultToCompare) {
+      console.log('gamerResult win ',gamerResult);
+    } else {
+      console.log('resultToCompare loose',resultToCompare);
+
+    }
+  }
+  const gameLoop = (rate) => {
+    
     if (count < data.length - 1) {
+      console.log([data[count], data[count + 1]]);
+      if (rate) {
+        winCount(rate,data[count + 1].vote_average)
+      } 
       setCount(count + 1);
       setDataToShow([]);
-      setDataToShow([data[count].title, data[count + 1].title]);
-      console.log('dataToShow -->', dataToShow);
+      setDataToShow([data[count], data[count + 1]]);
     }
+   
     // faire un else apres avec redirection fin de game 
   };
+
 
   return (
     <>
       <h1>win streak </h1>
       <div>{dataToShow && dataToShow.map((filmData) => {
         return (
-          <div key={filmData}>
-            <button onClick={gameLoop}>{filmData}</button>
+          <div key={filmData.id}>
+            <button onClick={() => gameLoop(filmData.vote_average)}>{filmData.title} {filmData.vote_average}</button>
           </div>
         )
-      })}</div> 
+      })}
+        {/* <button onClick={gameLoop}></button> */}
+      </div> 
 
     </>
   )
