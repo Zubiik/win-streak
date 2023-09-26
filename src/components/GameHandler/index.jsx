00d6fb436cb916536db56c9b5e22f3dbac688aed
Zubiik <elodiefrappat08@gmail.com>
2023-09-26 12:02:48 +0200

    feat/ create custom j=hook

--- src/components/GameHandler/index.jsx
+++ src/components/GameHandler/index.jsx
@@ -4,71 +4,20 @@ import SvgComponent from "../Arrow";
 import GameLoose from "../../pages/GameLoose";
 import Question from "../Question";
 import GameHeader from "../GameHeader";
+import useGameLogic from "./hooks/useGameLogic";
 
 const GameHandler = ({ setIsStarted, gameData, setGameData, gameTheme }) => {
-  // Mettre toute la logique dans un CustomHook. Creer un dossier hooks/ dans le dossier GameHandler/
+  const [
+    questions,
+    turn,
+    score,
+    userScore,
+    rightAnswer,
+    isLoose,
+    setIsLoose,
+    gameLoop,
+  ] = useGameLogic(gameData, setGameData, gameTheme);
 
-  const [questions, setQuestions] = useState([]);
-  const [turn, setTurn] = useState(0);
-  const [score, setScore] = useState(0);
-  const [rightAnswer, setRightAnswer] = useState(false);
-  const [isLoose, setIsLoose] = useState(false);
-
-  const userScore = localStorage.getItem(gameTheme);
-
-  const shuffle = () => {
-    //  Effectivement ta randomisation n'est pas bonne, normalement tu dois rentrer un chiffre
-    const arrayCopy = gameData.sort((a, b) => Math.random() - Math.random());
-    setGameData(arrayCopy);
-  };
-  useEffect(() => {
-    shuffle();
-  }, []);
-
-  useEffect(() => {
-    // Est ce que tu as essayer de tester ton jeu avec un tableau avec 3 entrées max ?
-    // Je pense que tu ne vérifie pas le cas ou tu es a la fin du tableau
-    if (gameData.length !== 0) {
-      setQuestions([gameData[turn], gameData[turn + 1]]);
-    }
-  }, [gameData, turn]);
-
-  const isRightAnswer = (userChoice) => {
-    if (
-      (questions[0].rate < questions[1].rate &&
-        userChoice === questions[1].rate) ||
-      (questions[0].rate > questions[1].rate &&
-        userChoice === questions[0].rate) ||
-      questions[0].rate === questions[1].rate
-    ) {
-      return true;
-    }
-    return false;
-  };
-
-  const gameLoop = (userChoice) => {
-    // Pk tu set RightAnswer ici a true ?
-    setRightAnswer(true);
-    // PK un timeout ??
-    setTimeout(() => {
-      const answer = isRightAnswer(userChoice);
-      if (answer) {
-        setScore(score + 1);
-        setTurn(turn + 1);
-      } else {
-        // set questions est trigger 2 fois
-        setIsLoose(true);
-        setScore(0);
-        if (score > userScore) {
-          localStorage.setItem(gameTheme, score);
-        }
-        setTurn(0);
-        shuffle();
-      }
-      // // Pk tu set RightAnswer ici a false ici ensuite ? ?
-      setRightAnswer(false);
-    }, 1000);
-  };
   return (
     <Container>
       <SvgComponent onClick={() => setIsStarted(false)} />
@@ -96,6 +45,7 @@ const GameHandler = ({ setIsStarted, gameData, setGameData, gameTheme }) => {
                     rightAnswer={rightAnswer}
                     gameLoop={gameLoop}
                     index={index}
+                    key={gameInfo.id}
                   />
                 );
               })
